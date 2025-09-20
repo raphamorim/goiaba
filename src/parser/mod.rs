4b6f8c47d79f35fd40c6aff995956dba0c204740
Raphael Amorim <rapha850@gmail.com>
2025-09-21 01:33:20 +0200

    start struct support

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -713,6 +713,13 @@ pub enum Expression {
     CallExpr(Box<Expression>, Vec<Expression>),
     IndexExpr(Box<Expression>, Box<Expression>),
     FieldAccessExpr(Box<Expression>, String),
+    StructLiteral(String, Vec<FieldValue>),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FieldValue {
+    pub name: String,
+    pub value: Expression,
 }
 
 #[derive(Debug, PartialEq)]
@@ -1502,7 +1509,16 @@ impl Parser {
         let expr = if self.check(TokenType::Identifier) {
             let identifier = self.current_token().lexeme.clone();
             self.advance(); // Consume identifier
-            Expression::Identifier(identifier)
+
+            // Check for struct literal: TypeName{field: value}
+            if self.check(TokenType::LBrace) {
+                self.advance(); // Consume '{'
+                let fields = self.parse_struct_literal_fields()?;
+                self.consume(TokenType::RBrace, "Expected '}' after struct fields")?;
+                Expression::StructLiteral(identifier, fields)
+            } else {
+                Expression::Identifier(identifier)
+            }
         } else if self.check(TokenType::IntLiteral) {
             let value = self
                 .current_token()
@@ -1545,6 +1561,31 @@ impl Parser {
         };
 
         self.parse_primary_suffix(expr)
+    }
+
+    fn parse_struct_literal_fields(&mut self) -> Result<Vec<FieldValue>, String> {
+        let mut fields = Vec::new();
+
+        if !self.check(TokenType::RBrace) {
+            loop {
+                let field_name = self.consume_identifier("Expected field name")?;
+                self.consume(TokenType::Colon, "Expected ':' after field name")?;
+                let field_value = self.parse_expression()?;
+
+                fields.push(FieldValue {
+                    name: field_name,
+                    value: field_value,
+                });
+
+                if !self.check(TokenType::Comma) {
+                    break;
+                }
+
+                self.advance(); // Consume comma
+            }
+        }
+
+        Ok(fields)
     }
 
     fn parse_primary_suffix(&mut self, expr: Expression) -> Result<Expression, String> {
@@ -2061,6 +2102,420 @@ mod tests {
             }
         } else {
             panic!("Expected type declaration");
+        }
+    }
+
+    #[test]
+    fn test_parse_struct_with_multiple_field_names() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            type Point struct {
+                x, y, z float64
+                name string
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        if let Declaration::Type(type_specs) = &program.declarations[0] {
+            if let TypeValue::Struct(fields) = &type_specs[0].type_value {
+                assert_eq!(fields.len(), 2);
+
+                // First field: x, y, z float64
+                assert_eq!(fields[0].names, vec!["x", "y", "z"]);
+                assert_eq!(fields[0].field_type, "float64");
+
+                // Second field: name string
+                assert_eq!(fields[1].names, vec!["name"]);
+                assert_eq!(fields[1].field_type, "string");
+            } else {
+                panic!("Expected struct type");
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_struct_with_tags() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            type User struct {
+                ID int "json:\"id\""
+                Name string "json:\"name\" xml:\"name\""
+                Email string "json:\"email,omitempty\""
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        if let Declaration::Type(type_specs) = &program.declarations[0] {
+            if let TypeValue::Struct(fields) = &type_specs[0].type_value {
+                assert_eq!(fields.len(), 3);
+
+                // ID field with tag
+                assert_eq!(fields[0].names, vec!["ID"]);
+                assert_eq!(fields[0].field_type, "int");
+                assert_eq!(fields[0].tag, Some("json:\"id\"".to_string()));
+
+                // Name field with tag
+                assert_eq!(fields[1].names, vec!["Name"]);
+                assert_eq!(fields[1].field_type, "string");
+                assert_eq!(
+                    fields[1].tag,
+                    Some("json:\"name\" xml:\"name\"".to_string())
+                );
+
+                // Email field with tag
+                assert_eq!(fields[2].names, vec!["Email"]);
+                assert_eq!(fields[2].field_type, "string");
+                assert_eq!(fields[2].tag, Some("json:\"email,omitempty\"".to_string()));
+            } else {
+                panic!("Expected struct type");
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_empty_struct() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            type Empty struct {}
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        if let Declaration::Type(type_specs) = &program.declarations[0] {
+            if let TypeValue::Struct(fields) = &type_specs[0].type_value {
+                assert_eq!(fields.len(), 0);
+            } else {
+                panic!("Expected struct type");
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_struct_literal() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            func main() {
+                p := Person{name: "John", age: 30}
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        if let Declaration::Function(func) = &program.declarations[0] {
+            if let Statement::ShortDeclStmt(names, values) = &func.body.statements[0] {
+                assert_eq!(names, &vec!["p"]);
+                assert_eq!(values.len(), 1);
+
+                if let Expression::StructLiteral(struct_name, fields) = &values[0] {
+                    assert_eq!(struct_name, "Person");
+                    assert_eq!(fields.len(), 2);
+
+                    // name field
+                    assert_eq!(fields[0].name, "name");
+                    if let Expression::StringLiteral(value) = &fields[0].value {
+                        assert_eq!(value, "John");
+                    } else {
+                        panic!("Expected string literal for name field");
+                    }
+
+                    // age field
+                    assert_eq!(fields[1].name, "age");
+                    if let Expression::IntLiteral(value) = &fields[1].value {
+                        assert_eq!(*value, 30);
+                    } else {
+                        panic!("Expected int literal for age field");
+                    }
+                } else {
+                    panic!("Expected struct literal");
+                }
+            } else {
+                panic!("Expected short declaration statement");
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_empty_struct_literal() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            func main() {
+                empty := Empty{}
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        if let Declaration::Function(func) = &program.declarations[0] {
+            if let Statement::ShortDeclStmt(names, values) = &func.body.statements[0] {
+                assert_eq!(names, &vec!["empty"]);
+
+                if let Expression::StructLiteral(struct_name, fields) = &values[0] {
+                    assert_eq!(struct_name, "Empty");
+                    assert_eq!(fields.len(), 0);
+                } else {
+                    panic!("Expected struct literal");
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_struct_field_access() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            func main() {
+                name := person.name
+                age := person.address.zipcode
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        if let Declaration::Function(func) = &program.declarations[0] {
+            // First statement: name := person.name
+            if let Statement::ShortDeclStmt(names, values) = &func.body.statements[0] {
+                assert_eq!(names, &vec!["name"]);
+
+                if let Expression::FieldAccessExpr(obj, field) = &values[0] {
+                    if let Expression::Identifier(obj_name) = &**obj {
+                        assert_eq!(obj_name, "person");
+                    }
+                    assert_eq!(field, "name");
+                } else {
+                    panic!("Expected field access expression");
+                }
+            }
+
+            // Second statement: age := person.address.zipcode (chained field access)
+            if let Statement::ShortDeclStmt(names, values) = &func.body.statements[1] {
+                assert_eq!(names, &vec!["age"]);
+
+                if let Expression::FieldAccessExpr(obj, field) = &values[0] {
+                    if let Expression::FieldAccessExpr(inner_obj, inner_field) = &**obj {
+                        if let Expression::Identifier(obj_name) = &**inner_obj {
+                            assert_eq!(obj_name, "person");
+                        }
+                        assert_eq!(inner_field, "address");
+                    }
+                    assert_eq!(field, "zipcode");
+                } else {
+                    panic!("Expected chained field access expression");
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_struct_assignment() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            func main() {
+                person.name = "Jane"
+                person.age = 25
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        if let Declaration::Function(func) = &program.declarations[0] {
+            // First assignment: person.name = "Jane"
+            if let Statement::AssignmentStmt(left, right) = &func.body.statements[0] {
+                assert_eq!(left.len(), 1);
+                assert_eq!(right.len(), 1);
+
+                if let Expression::FieldAccessExpr(obj, field) = &left[0] {
+                    if let Expression::Identifier(obj_name) = &**obj {
+                        assert_eq!(obj_name, "person");
+                    }
+                    assert_eq!(field, "name");
+                }
+
+                if let Expression::StringLiteral(value) = &right[0] {
+                    assert_eq!(value, "Jane");
+                }
+            }
+
+            // Second assignment: person.age = 25
+            if let Statement::AssignmentStmt(left, right) = &func.body.statements[1] {
+                if let Expression::FieldAccessExpr(obj, field) = &left[0] {
+                    if let Expression::Identifier(obj_name) = &**obj {
+                        assert_eq!(obj_name, "person");
+                    }
+                    assert_eq!(field, "age");
+                }
+
+                if let Expression::IntLiteral(value) = &right[0] {
+                    assert_eq!(*value, 25);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_nested_struct_types() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            type Address struct {
+                street string
+                city string
+                zipcode int
+            }
+            
+            type Person struct {
+                name string
+                age int
+                address Address
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        assert_eq!(program.declarations.len(), 2);
+
+        // Address struct
+        if let Declaration::Type(type_specs) = &program.declarations[0] {
+            assert_eq!(type_specs[0].name, "Address");
+            if let TypeValue::Struct(fields) = &type_specs[0].type_value {
+                assert_eq!(fields.len(), 3);
+                assert_eq!(fields[0].names, vec!["street"]);
+                assert_eq!(fields[0].field_type, "string");
+                assert_eq!(fields[1].names, vec!["city"]);
+                assert_eq!(fields[1].field_type, "string");
+                assert_eq!(fields[2].names, vec!["zipcode"]);
+                assert_eq!(fields[2].field_type, "int");
+            }
+        }
+
+        // Person struct with Address field
+        if let Declaration::Type(type_specs) = &program.declarations[1] {
+            assert_eq!(type_specs[0].name, "Person");
+            if let TypeValue::Struct(fields) = &type_specs[0].type_value {
+                assert_eq!(fields.len(), 3);
+                assert_eq!(fields[2].names, vec!["address"]);
+                assert_eq!(fields[2].field_type, "Address");
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_interface_type() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            type Writer interface {
+                Write(data string) int
+                Close() error
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        if let Declaration::Type(type_specs) = &program.declarations[0] {
+            assert_eq!(type_specs.len(), 1);
+            assert_eq!(type_specs[0].name, "Writer");
+
+            if let TypeValue::Interface(methods) = &type_specs[0].type_value {
+                assert_eq!(methods.len(), 2);
+
+                // Write method
+                assert_eq!(methods[0].name, "Write");
+                assert_eq!(methods[0].parameters.len(), 1);
+                assert_eq!(methods[0].parameters[0].name, "data");
+                assert_eq!(methods[0].parameters[0].param_type, "string");
+                assert_eq!(methods[0].return_type, Some("int".to_string()));
+
+                // Close method
+                assert_eq!(methods[1].name, "Close");
+                assert_eq!(methods[1].parameters.len(), 0);
+                assert_eq!(methods[1].return_type, Some("error".to_string()));
+            } else {
+                panic!("Expected interface type");
+            }
+        } else {
+            panic!("Expected type declaration");
+        }
+    }
+
+    #[test]
+    fn test_parse_complex_struct_usage() {
+        let mut parser = Parser::new(
+            r#"
+            package main
+            
+            type Point struct {
+                x, y float64
+            }
+            
+            func main() {
+                p1 := Point{x: 1.0, y: 2.0}
+                p2 := Point{}
+                distance := p1.x - p2.x
+                p1.y = p1.y + 5.0
+            }
+        "#,
+        );
+        let program = parser.parse().unwrap();
+
+        assert_eq!(program.declarations.len(), 2);
+
+        // Verify Point struct declaration
+        if let Declaration::Type(type_specs) = &program.declarations[0] {
+            assert_eq!(type_specs[0].name, "Point");
+        }
+
+        // Verify function with struct usage
+        if let Declaration::Function(func) = &program.declarations[1] {
+            assert_eq!(func.body.statements.len(), 4);
+
+            // p1 := Point{x: 1.0, y: 2.0}
+            if let Statement::ShortDeclStmt(_, values) = &func.body.statements[0] {
+                if let Expression::StructLiteral(name, fields) = &values[0] {
+                    assert_eq!(name, "Point");
+                    assert_eq!(fields.len(), 2);
+                }
+            }
+
+            // p2 := Point{}
+            if let Statement::ShortDeclStmt(_, values) = &func.body.statements[1] {
+                if let Expression::StructLiteral(name, fields) = &values[0] {
+                    assert_eq!(name, "Point");
+                    assert_eq!(fields.len(), 0);
+                }
+            }
+
+            // distance := p1.x - p2.x
+            if let Statement::ShortDeclStmt(names, values) = &func.body.statements[2] {
+                assert_eq!(names, &vec!["distance"]);
+                if let Expression::BinaryExpr(left, BinaryOp::Sub, right) = &values[0] {
+                    // Verify both sides are field access expressions
+                    if let Expression::FieldAccessExpr(_, field) = &**left {
+                        assert_eq!(field, "x");
+                    }
+                    if let Expression::FieldAccessExpr(_, field) = &**right {
+                        assert_eq!(field, "x");
+                    }
+                }
+            }
+
+            // p1.y = p1.y + 5.0
+            if let Statement::AssignmentStmt(left, right) = &func.body.statements[3] {
+                if let Expression::FieldAccessExpr(_, field) = &left[0] {
+                    assert_eq!(field, "y");
+                }
+                if let Expression::BinaryExpr(_, BinaryOp::Add, _) = &right[0] {
+                    // Correct binary expression
+                }
+            }
         }
     }
 }
